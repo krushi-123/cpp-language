2c6e20c09ef827e2bc5a20bb00c4eb24e4e231f9
Krushali <sq.krushali@gmail.com>
2023-07-13 10:50:56 +0530

    cpp-language

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout<<"Hello World";
+    cout<<"\nEnter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
+    cout<<"Product of a and b :"<<a*b;
+}
