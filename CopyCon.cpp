ed4810f2687a624b22c9febde7d270d0aca1701c
Krushali <sq.krushali@gmail.com>
2023-08-03 11:10:45 +0530

    Copy Constructor

---
+++ CopyCon.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Test
+{
+    int a, b;
+
+    public :
+
+    Test()
+    {
+        a = 5, b = 10;
+        cout<<"Test class called...."<<endl;
+        cout<<"product of a and b is : "<<a*b<<endl;
+    }
+    Test (Test &x)
+    {
+        cout<<"Copy Constructor...."<<endl;
+        cout<<"Product of a and b is : "<<x.a*x.b<<endl;
+    }
+};
+
+int main()
+{
+    Test t;
+    Test b(t);
+}
